a0baf0077d1741cc3c3c27ddcf99761085a20d2a
Polymorphose <51100497+Polymorphose@users.noreply.github.com>
2019-05-28 02:28:27 +0200

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,36 @@
+# name: Wakatoo - discourse
+# about: ajoute fonction wakatoo emoji
+# version: 0.1
+# authors: Polymorphose
+# url: https://github.com/Polymorphose/Wakatoo-discourse
+
+enabled_site_setting :wakatoo_discourse_enabled
+
+register_asset "stylesheets/wakatoo_discourse.css"
+
+# Wakatoo
+register_emoji "a", "/plugins/Wakatoo-discourse/images/a.jpg"
+register_emoji "e", "/plugins/Wakatoo-discourse/images/e.jpg"
+register_emoji "FLECHE", "/plugins/Wakatoo-discourse/images/FLECHE.jpg"
+register_emoji "go", "/plugins/Wakatoo-discourse/images/go.jpg"
+register_emoji "i", "/plugins/Wakatoo-discourse/images/i.jpg"
+register_emoji "ka", "/plugins/Wakatoo-discourse/images/ka.jpg"
+register_emoji "ke", "/plugins/Wakatoo-discourse/images/ke.jpg"
+register_emoji "ki", "/plugins/Wakatoo-discourse/images/ki.jpg"
+register_emoji "ko", "/plugins/Wakatoo-discourse/images/ko.jpg"
+register_emoji "kouf", "/plugins/Wakatoo-discourse/images/kouf.jpg"
+register_emoji "o", "/plugins/Wakatoo-discourse/images/o.jpg"
+register_emoji "SILENCE", "/plugins/Wakatoo-discourse/images/SILENCE.jpg"
+register_emoji "ta", "/plugins/Wakatoo-discourse/images/ta.jpg"
+register_emoji "te", "/plugins/Wakatoo-discourse/images/te.jpg"
+register_emoji "ti", "/plugins/Wakatoo-discourse/images/ti.jpg"
+register_emoji "to", "/plugins/Wakatoo-discourse/images/to.jpg"
+register_emoji "toc", "/plugins/Wakatoo-discourse/images/toc.jpg"
+register_emoji "tog", "/plugins/Wakatoo-discourse/images/tog.jpg"
+register_emoji "TON", "/plugins/Wakatoo-discourse/images/TON.jpg"
+register_emoji "too_1", "/plugins/Wakatoo-discourse/images/too_1.jpg"
+register_emoji "too_2", "/plugins/Wakatoo-discourse/images/too_2.jpg"
+register_emoji "tooh", "/plugins/Wakatoo-discourse/images/tooh.jpg"
+register_emoji "tor", "/plugins/Wakatoo-discourse/images/tor.jpg"
+register_emoji "WA_joues", "/plugins/Wakatoo-discourse/images/WA_joues.jpg"
+register_emoji "WON_joues", "/plugins/Wakatoo-discourse/images/WON_joues.jpg"
