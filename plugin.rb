bdd4b09cebb6109547f5b2be3994a1fbce4007f3
polymorphose <51100497+polymorphose@users.noreply.github.com>
2019-09-29 00:55:22 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: wakatoo-discourse
 # about: ajoute fonction wakatoo emoji
-# version: 0.1.1
+# version: 0.1.2
 # authors: polymorphose
 # url: https://github.com/Polymorphose/Wakatoo-discourse
 
